f25287bf8376acabc9c2e7ddcd734ddf95d58f63
Victor Mbachu <victor.c.mbachu@gmail.com>
2024-08-05 23:28:14 +0100

    resolved invoice submittion

--- resources/js/views/pages/invoices/InvoicesForm.vue
+++ resources/js/views/pages/invoices/InvoicesForm.vue
@@ -99,7 +99,23 @@ const submitInvoice = async (event, action = null) => {
     }
 
   } catch (error) {
-    handleErrors(error, errors.value);
+    if (error.response) {
+
+      if(config.APP_ENV === 'local'){
+        console.log("Error response", error.response);
+      }
+
+      if (Object.keys(error.response?.data?.errors || {}).length > 0) {
+        errors.value = error.response?.data?.errors;
+        if(config.APP_ENV === 'local'){
+          console.log("Validation Errors", error.value);
+        }
+      }
+
+      if (error.response?.data?.server_error) {
+        errors.value.server_error = 'Server error. Please try again later or contact your admin.';
+      }
+    }
   }
 
   loading.value = false;
